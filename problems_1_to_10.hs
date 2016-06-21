be0bf6d462ed86858e417718b3dc91f31b0d2076
Arvind Iyer <mail@arvindiyer.xyz>
2016-06-21 19:06:57 +0800

    Finished eliminate consecutive duplicates (#8)

--- problems_1_to_10.hs
+++ problems_1_to_10.hs
@@ -8,46 +8,49 @@ myLast (_:x) = last x
 secondLast :: [a] -> a
 secondLast [] = error "Empty list"
 secondLast [x] =  error "List does is too short"
-secondLast (_:x) = last ( init x ) 
+secondLast (_:x) = last ( init x )
 
--- Problem 3: Find the K'th element of a list. 
+-- Problem 3: Find the K'th element of a list.
 -- 	      The first element in the list is number 1.
 elementAt :: Int -> [a] -> a
-elementAt k _ 
+elementAt k _
 	| k < 1  = error "Index not a positive integer"
 elementAt k list = list !! (k-1)
 
 -- Problem 4: Find the number of elements of a list.
 myLength :: [a] -> Int
-myLength list = countElems list 0 
-	where 
+myLength list = countElems list 0
+	where
 		-- Pop and count until list is empty
 		countElems [] t = t
 		countElems (x:xs) t = countElems xs (t+1)
 
--- Problem 5: Reverse a list 
+-- Problem 5: Reverse a list
 -- I'll avoid using the reverse command, getting bored of using inbuilts
 -- Feels like a cheat
 myReverse :: [a] -> [a]
 myReverse list = moveAndReverse list []
-	where 
-		-- Popping first element and pushing it into the 
+	where
+		-- Popping first element and pushing it into the
 		-- first position of the reversed list
 		-- is repeated till the original is empty and reversed is filled
 		moveAndReverse [] list = list
 		moveAndReverse (x:xs) reversed = moveAndReverse xs (x:reversed)
 
 -- Problem 6: Find out whether a list is a palindrome
-isPalindrome :: Eq a => [a] -> Bool 
+isPalindrome :: Eq a => [a] -> Bool
 -- Reusing code heh
-isPalindrome list = (list  == (myReverse list)) 
+isPalindrome list = (list  == (myReverse list))
 
 -- Problem 7: Flatten a nested list structure
--- Solving by first defining a list that can have either elements or lists of 
+-- Solving by first defining a list that can have either elements or lists of
 -- these nested lists
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
 flatten (Elem x) = [x]
-flatten (List x) = concat (map flatten x) 
+flatten (List x) = concat (map flatten x)
 
 -- Problem 8: Eliminate duplicates
+compress :: Eq a => [a] -> [a]
+compress [] = []
+compress (x:xs) = x:compress(dropWhile (==x) xs)
