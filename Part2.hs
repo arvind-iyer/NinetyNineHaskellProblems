7d29fa54efa87f6d3772b7e7e1dc9952e092d53f
Arvind Iyer <arvindi.me@gmail.com>
2016-08-16 17:37:16 +0530

    Completed problems 11-20

--- Part2.hs
+++ Part2.hs
@@ -38,3 +38,50 @@ toEncodeList y
 
 encodeDirect :: Eq a => [a] -> [EncodeList a]
 encodeDirect x = map toEncodeList (encodeDirect' x)
+
+
+-- Problem 14: Duplicate a list
+duplicate :: [a] -> [a]
+duplicate [] = []
+duplicate (x:xs) = x:x:(duplicate xs)
+
+-- Problem 15: Replicate each element in a list a given number of times
+-- Prelude imports another replicate function so naming this repli
+
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n
+  | n < 0 = error "Array cannot be replicated negative times"
+  | otherwise = (repli' x n) ++ (repli xs n)
+                 where repli' x n
+                         | n <= 0 = []
+                         | otherwise = x : (repli' x (pred n))
+
+-- Problem 16: Drop every N'th element from a list.
+dropEvery :: [a] -> Int -> [a]
+dropEvery x n
+  | n <= 0 = x
+  | length x >= n = init (take n x) ++ (dropEvery (drop n x) n)
+  | otherwise = x
+
+-- Problem 17: Split a list into two parts; the length of the first part is given.
+split :: [a] -> Int -> ([a], [a])
+split x n = ((take n x), (drop n x))
+
+-- Problem 18:  Extract a slice from a list.
+-- Note: Both indices inclusive, indexing starts from 1.
+slice :: [a] -> Int -> Int -> [a]
+slice a i k
+  | k < i = slice a k i
+  | otherwise = take (k - (pred i)) (drop (pred i) a)
+
+-- Problem 19:  Rotate a list N places to the left.
+rotate :: [a] -> Int -> [a]
+rotate x n
+  | n > 0 = drop n x ++ take n x
+  | n < 0 = rotate x (length x + n)
+  | otherwise = x
+
+-- Problem 20: Remove the K'th element from a list.
+removeAt :: Int -> [a] -> (a, [a])
+removeAt n x = ( x !! (pred n), take (pred n) x ++ (drop n x))
